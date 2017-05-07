60bdf3ba79226d04a3f89a2d6c1481a6f21523c1
podhmo <ababjam61+github@gmail.com>
2017-05-07 18:27:05 +0900

    add test for struct writer

--- goaway/tests/test_writer.py
+++ goaway/tests/test_writer.py
@@ -21,10 +21,31 @@ class WriterTests(unittest.TestCase):
         actual = str(r.writer.write_function(Add, f, r.m))
         expected = textwrap.dedent(
             """
-        // Add : +
-        func Add(x int, y int) int {
-        \treturn x + y
-        }
-"""
+            // Add : +
+            func Add(x int, y int) int {
+            	return x + y
+            }
+            """
+        )
+        self.assertEqual(actual.strip(), expected.strip())
+
+    def test_struct(self):
+        r = self._getRepository()
+        f = self._makeFile(r, "main.go")
+        time = f.import_("time")
+
+        Person = f.struct("Person", comment="人間")
+        Person.define_field("Name", f.int)
+        Person.define_field("BirthDay", time.type("Time"))
+
+        actual = str(r.writer.write_struct(Person, f, r.m))
+        expected = textwrap.dedent(
+            """
+            // Person : 人間
+            type Person struct {
+            	Name int
+            	BirthDay time.Time
+            }
+            """
         )
         self.assertEqual(actual.strip(), expected.strip())
